4c09af068301b60b9e90e1027cd398ef732cf180
Sergiikhl <sergiykhalin@gmail.com>
2025-06-08 11:57:46 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-// pages/_app.js
+
 import "../styles/globals.css";
 import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 via-white to-purple-100 text-gray-800">
+    <div>
       <NavBar />
-      <Component {...pageProps} />
+      <div className="pt-20 px-4">
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
