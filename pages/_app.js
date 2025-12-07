26bd271f09272c80d95ad31587bbf608457249d2
Sergiikhl <sergiykhalin@gmail.com>
2025-12-07 16:17:16 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,11 @@ import NavBar from "../components/NavBar";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="bg-slate-950 text-slate-100 min-h-screen">
       <NavBar />
-      <main className="min-h-screen bg-slate-950 text-slate-100 pt-16">
+      <main className="max-w-6xl mx-auto px-4 pb-16 pt-6">
         <Component {...pageProps} />
       </main>
-    </>
+    </div>
   );
 }
