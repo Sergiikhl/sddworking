106d46b9bb7182e1e72ae3f15131df79523a9cce
Sergiikhl <sergiykhalin@gmail.com>
2025-12-07 16:21:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-// pages/_app.js
 import "../styles/globals.css";
 import NavBar from "../components/NavBar";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="bg-slate-950 text-slate-100 min-h-screen">
+    <div className="bg-white text-gray-800 min-h-screen">
       <NavBar />
-      <main className="max-w-6xl mx-auto px-4 pb-16 pt-6">
+      <main className="max-w-6xl mx-auto px-4 py-10">
         <Component {...pageProps} />
       </main>
     </div>
